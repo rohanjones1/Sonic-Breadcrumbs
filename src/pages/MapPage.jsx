cfaa9bb5f668c398a52792ef06f39e9812810ee1
rohanjones1 <rohanjjones@gmail.com>
2026-03-01 16:21:36 -0800

    Added handle drop to not work if not signed in

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,13 +1,19 @@
 import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./MapPage.css";
 import DropSongModal from "./DropSongModal";
+import { db } from "../firebase";
+import { collection, addDoc } from "firebase/firestore";
+import { auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
 function MapPage() {
   const mapContainer = useRef(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const map = new mapboxgl.Map({
@@ -33,10 +39,29 @@ function MapPage() {
 
   const [showModal, setShowModal] = useState(false);
 
-  const handleDrop = (song) => {
-    console.log("Dropped song:", song);
-    setShowModal(false);
-    // we'll save to Firebase here next
+  const handleDrop = async (song) => {
+    if (!auth.currentUser) {
+      navigate("/login");
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(async (pos) => {
+      const { latitude, longitude } = pos.coords;
+
+      await addDoc(collection(db, "drops"), {
+        trackName: song.trackName,
+        artistName: song.artistName,
+        artwork: song.artworkUrl100,
+        previewUrl: song.previewUrl,
+        lat: latitude,
+        lng: longitude,
+        timestamp: new Date(),
+        userId: auth.currentUser?.uid,
+        userName: auth.currentUser?.displayName,
+      });
+
+      setShowModal(false);
+    });
   };
 
   return (
